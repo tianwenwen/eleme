1abee065beeedd3b28bb69214de151a6433dda7f
vivian <tianwenwen555@sina.com>
2017-12-25 17:40:42 +0800

    增加vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
@@ -9,6 +10,7 @@ import '@/common/less/index.less'
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
